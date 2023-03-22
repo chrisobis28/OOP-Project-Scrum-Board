0835636226f7a09addea960b8e1e0d3404b341ba
Tudor Andrei Coman <T.A.Coman@student.tudelft.nl>
2023-03-22 22:55:45 +0100

    Merge branch 'Merge_request_branch' into Merge_request_branch

--- commons/src/main/java/commons/Board.java
+++ commons/src/main/java/commons/Board.java
@@ -1,112 +1,72 @@
 package commons;
 
-
+import java.util.HashSet;
+import java.util.Set;
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
-import javax.persistence.*;
-import java.util.HashSet;
-import java.util.Set;
-
-
 @Entity
-
 public class Board {
 
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  public long id;
 
-    @Id
+  public String boardName;
 
-    @GeneratedValue(strategy = GenerationType.AUTO)
+  @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
+  public Set<Cardlist> cardlistList;
 
-    public long id;
+  @OneToMany(mappedBy = "board")
+  public Set<Tag> tagList;
 
+  public String boardBackgroundColour = "#FFFFFF";
 
-    public String boardName;
+  public String listsBackgroundColour = "#FFFFFF";
 
+  public Board(){
 
-    @OneToMany(mappedBy = "board", cascade = CascadeType.ALL)
+  }
 
-    public Set<Cardlist> cardlistList;
+  public Board(String boardName){
+    this.boardName = boardName;
+    tagList = new HashSet<>();
+    cardlistList = new HashSet<>();
+  }
 
-
-    @OneToMany(mappedBy = "board")
-
-    public Set<Tag> tagList;
-
-
-    public String boardBackgroundColour = "#FFFFFF";
-
-
-    public String listsBackgroundColour = "#FFFFFF";
-
-
-    public Board(){
-
-
-    }
+  public void setBoardName(String boardName) {
+    this.boardName = boardName;
+  }
 
 
-    public Board(String boardName){
+  public void setBoardBackgroundColour(String boardBackgroundColour) {
+    this.boardBackgroundColour = boardBackgroundColour;
+  }
 
-        this.boardName = boardName;
+  public void setListsBackgroundColour(String listsBackgroundColour) {
+    this.listsBackgroundColour = listsBackgroundColour;
+  }
 
-        tagList = new HashSet<>();
+  @Override
+  public boolean equals(Object obj){
+    return EqualsBuilder.reflectionEquals(this, obj);
+  }
 
-        cardlistList = new HashSet<>();
+  @Override
+  public int hashCode(){
+    return HashCodeBuilder.reflectionHashCode(this);
+  }
 
-    }
-
-
-    public void setBoardName(String boardName) {
-
-        this.boardName = boardName;
-
-    }
-
-
-
-    public void setBoardBackgroundColour(String boardBackgroundColour) {
-
-        this.boardBackgroundColour = boardBackgroundColour;
-
-    }
-
-
-    public void setListsBackgroundColour(String listsBackgroundColour) {
-
-        this.listsBackgroundColour = listsBackgroundColour;
-
-    }
-
-
-    @Override
-
-    public boolean equals(Object obj){
-
-        return EqualsBuilder.reflectionEquals(this, obj);
-
-    }
-
-
-    @Override
-
-    public int hashCode(){
-
-        return HashCodeBuilder.reflectionHashCode(this);
-
-    }
-
-
-    @Override
-
-    public String toString(){
-
-        return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
-
-    }
-
+  @Override
+  public String toString(){
+    return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
+  }
 }
-
-
--- commons/src/test/java/commons/BoardTest.java
+++ commons/src/test/java/commons/BoardTest.java
@@ -1,112 +1,62 @@
 package commons;
 
-
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-
 public class BoardTest {
 
-
   @Test
-
   public void checkConstructor(){
-
     var p = new Board("example-name");
-
     assertEquals("example-name", p.boardName);
-
   }
 
-
   @Test
-
   public void checkBoardNameSetter() {
-
     var p = new Board("example-name");
-
     p.setBoardName("example");
-
     assertEquals("example", p.boardName);
-
   }
 
-
   @Test
-
   public void checkBoardColourSetter(){
-
     var p = new Board("some-name");
-
     assertEquals("#FFFFFF", p.boardBackgroundColour);
-
     p.setBoardBackgroundColour("#ABCDEF");
-
     assertEquals("#ABCDEF", p.boardBackgroundColour);
-
   }
 
-
   @Test
-
   public void checkListColourSetter(){
-
     var p = new Board("some-name");
-
     assertEquals("#FFFFFF", p.listsBackgroundColour);
-
     p.setListsBackgroundColour("#ABCDEF");
-
     assertEquals("#ABCDEF", p.listsBackgroundColour);
-
   }
 
-
   @Test
-
   public void equalsHashCode(){
-
     var a = new Board("a");
-
     var b = new Board("a");
-
     assertEquals(a,b);
-
     assertEquals(a.hashCode(), b.hashCode());
-
   }
 
-
   @Test
-
   public void notEqualsHashCode(){
-
     var a = new Board("a");
-
     var b = new Board("a");
-
     a.setListsBackgroundColour("#AAABBB");
-
     assertNotEquals(a, b);
-
     assertNotEquals(a.hashCode(), b.hashCode());
-
   }
-
 
   @Test
-
   public void hasToString(){
-
     var actual = new Board("board-name").toString();
-
     assertTrue(actual.contains(Board.class.getSimpleName()));
-
     assertTrue(actual.contains("\n"));
-
     assertTrue(actual.contains("boardName"));
-
   }
-
 }
