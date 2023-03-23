8a73100a69b0fb00b3a389eee6d404b3e96d315f
Andrei Ionita <A.Ionita@student.tudelft.nl>
2023-03-23 20:52:39 +0100

    Added getName method to the Board class and a test for this method. Added JavaDoc to the Board class.

--- commons/src/main/java/commons/Board.java
+++ commons/src/main/java/commons/Board.java
@@ -13,6 +13,9 @@ import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 import org.apache.commons.lang3.builder.ToStringStyle;
 
+/**
+ * Board class.
+ */
 @Entity
 public class Board {
 
@@ -32,41 +35,78 @@ public class Board {
 
   public String listsBackgroundColour = "#FFFFFF";
 
-  public Board(){
+  /**
+   * Default board constructor.
+   */
+  public Board() {}
 
-  }
-
-  public Board(String boardName){
+  /**
+   * Creates a new Board with a given name.
+   * @param boardName String representing a new board's name
+   */
+  public Board(String boardName) {
     this.boardName = boardName;
     tagList = new HashSet<>();
     cardlistList = new HashSet<>();
   }
 
+  /**
+   * Sets a board's name to a given string.
+   * @param boardName String to become the board's new name
+   */
   public void setBoardName(String boardName) {
     this.boardName = boardName;
   }
 
+  /**
+   * Return the board's name.
+   * @return String that represents the board's name
+   */
+  public String getName() {
+    return this.boardName;
+  }
 
+  /**
+   * Sets the board's background colour to a given colour.
+   * @param boardBackgroundColour String that represents the new colour of the board's background.
+   */
   public void setBoardBackgroundColour(String boardBackgroundColour) {
     this.boardBackgroundColour = boardBackgroundColour;
   }
 
+  /**
+   * Sets the lists' background colour to a given colour.
+   * @param listsBackgroundColour String that represents the new colour of the lists' background.
+   */
   public void setListsBackgroundColour(String listsBackgroundColour) {
     this.listsBackgroundColour = listsBackgroundColour;
   }
 
+  /**
+   * Checks if a given board is the same as this one.
+   * @param obj The given board to be compared to
+   * @return true if the boards are equal, false otherwise
+   */
   @Override
-  public boolean equals(Object obj){
+  public boolean equals(Object obj) {
     return EqualsBuilder.reflectionEquals(this, obj);
   }
 
+  /**
+   * Returns a hashcode for the board.
+   * @return Integer representing a hashcode for the board
+   */
   @Override
-  public int hashCode(){
+  public int hashCode() {
     return HashCodeBuilder.reflectionHashCode(this);
   }
 
+  /**
+   * Returns this board represented as a String.
+   * @return String representation of this board
+   */
   @Override
-  public String toString(){
+  public String toString() {
     return ToStringBuilder.reflectionToString(this, ToStringStyle.MULTI_LINE_STYLE);
   }
 }
