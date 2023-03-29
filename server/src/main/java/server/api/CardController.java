80d286ada18d12f125cac1e2dedcdfa19e7bbe0d
Chris Obis <c.obis-1@student.tudelft.nl>
2023-03-29 19:59:07 +0200

    Removed unused imports

--- server/src/main/java/server/api/CardController.java
+++ server/src/main/java/server/api/CardController.java
@@ -1,6 +1,5 @@
 package server.api;
 
-import commons.Board;
 import commons.Card;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- server/src/main/java/server/database/CardRepository.java
+++ server/src/main/java/server/database/CardRepository.java
@@ -2,8 +2,5 @@ package server.database;
 
 import commons.Card;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.List;
-
 public interface CardRepository extends JpaRepository<Card, Long> {
 }
