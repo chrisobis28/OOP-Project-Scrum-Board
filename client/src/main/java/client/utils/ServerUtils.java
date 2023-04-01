42389aa51a2ce632e58d4ca5fd57eacf9d9082f7
Secuiu Stefan Maniu <secuiu-1@student.tudelft.nl>
2023-04-01 19:45:40 +0200

    Missed unused import.

--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -30,7 +30,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.List;
-import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.function.Consumer;
