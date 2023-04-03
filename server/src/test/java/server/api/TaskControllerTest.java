7f4f5e81741f458b43029cd4904a57ac8318474c
Tudor Andrei Coman <T.A.Coman@student.tudelft.nl>
2023-04-03 19:11:41 +0200

    Save changes

--- server/src/test/java/server/api/TaskControllerTest.java
+++ server/src/test/java/server/api/TaskControllerTest.java
@@ -6,10 +6,16 @@ import commons.Card;
 import commons.Cardlist;
 import commons.Task;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
 public class TaskControllerTest {
     private TestTaskRepository repo;
-    private TaskController cardController;
+    private TaskController taskController;
 
     private Board z;
     private Cardlist a ;
@@ -20,12 +26,44 @@ public class TaskControllerTest {
     public void setup() {
         repo = new TestTaskRepository();
 
-        TaskController taskController= new TaskController(repo);
+        taskController= new TaskController(repo);
         a = new Cardlist("a");
         x = new Card("x", "X");
         y = new Card("y", "y");
         z = new Board("z");
         t = new Task("t");
         s = new Task("s");
+    }
+
+    @Test
+    public void testAddAll(){
+        taskController.add(t);
+        taskController.add(s);
+        assertEquals(taskController.getAll(), List.of(t, s));
+    }
+
+    @Test
+    public void testGetNegativeID(){
+        assertEquals(taskController.getById(-1).getStatusCode(), BAD_REQUEST);
+    }
+
+
+    @Test
+    public void testGetMissingCardlist(){
+        long ID = x.getId();
+        assertEquals(taskController.getById(ID).getStatusCode(), BAD_REQUEST);
+    }
+
+    @Test
+    public void testAddNullDescriptionTask(){
+        Task w = new Task(null);
+        assertEquals(BAD_REQUEST, taskController.add(w).getStatusCode());
+
+    }
+
+    @Test
+    public void testAddNullTask(){
+        Task w = null;
+        assertEquals(BAD_REQUEST, taskController.add(w).getStatusCode());
     }
 }
